995e48f671e5d96c132ff2a951ba87d41c804d3a
Tiago Mateus Carvalho <12384584677@campus.machado.mch>
2022-12-05 08:30:14 -0300

    initial commit

---
+++ src/fuzzyFrenagem/Fuzzy.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fuzzyFrenagem;
+
+
+
+/**
+ *
+ * @author 12384584677
+ */
+public class Fuzzy {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FuzzyInterface fuzy = new FuzzyInterface();
+        fuzy.setVisible(true);
+        // TODO code application logic here
+    }
+    
+}
